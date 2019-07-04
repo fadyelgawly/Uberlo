14c8937729e98d7c0c3483bbcd0d06f4b7ebd5ee
Fady Hanna <fadyhanna@aucegypt.edu>
2019-07-04 18:13:36 +0200

    Request ride updated

--- frontend/src/RequestRide.js
+++ frontend/src/RequestRide.js
@@ -1,22 +1,56 @@
 import React , { Component } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 
 export class RequestRide extends Component {
     render(){
         return(
-
-    <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Your location
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Masr El Gedida</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Tagamoa</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Zamalek</Dropdown.Item>
-        </Dropdown.Menu>
-    </Dropdown>
-
+    <div>
+        <h1>
+            Hello firstname lastname
+        </h1>
+        <Container>
+            <Grid>   
+            <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        From
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Masr El Gedida</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Tagamoa</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Zamalek</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        To
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Masr El Gedida</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Tagamoa</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Zamalek</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary">
+                Request Ride
+                </Button>
+            </Grid> 
+        </Container>
+    </div>
         )
     }
 }
--- frontend/src/rider/TripRequest.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const TripRequest = () => (
-    <div>
-        <h2> Drive</h2>
-        ABOUTABOUTABOUTABOUTABOUTABOUTABOUTHome page hereHome page hereHome page hereHome page hereHome page hereHome page here 
-    </div>
-)
